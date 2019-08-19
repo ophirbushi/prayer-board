8869bd98a380f06c5b4fb9be6e51be1bfa304c6e
Ophir Bushinsky <ophirbushi@gmail.com>
2019-08-19 11:04:23 +0300

    add prayerRequestIds to board model

--- server/src/db/board.js
+++ server/src/db/board.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const boardSchema = new mongoose.Schema({
   adminUserId: String,
   userIds: [String],
+  prayerRequestIds: [String]
 });
 
 const Board = mongoose.model('Board', boardSchema);
 
 module.exports = {
-  Board,
+  Board
 };
