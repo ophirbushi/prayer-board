3df250db6bfef3194e2f5e8246a5284243ada0ff
Ophir Bushinsky <ophirbushi@gmail.com>
2019-08-21 16:37:10 +0300

    create board

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,42 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef, OnDestroy } from '@angular/core';
 import { BoardService } from '../shared/board.service';
 import { AppState } from '../app-state';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { User, Board } from '../shared/models';
 import { ActivatedRoute, Router } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, takeUntil, take } from 'rxjs/operators';
 import { Toast } from '../shared/lib/toast/toast.service';
+import { MatDialog } from '@angular/material';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent implements OnInit, OnDestroy {
+  @ViewChild('newBoardPopupTpl', { static: true }) newBoardPopupTpl: TemplateRef<any>;
   user$: Observable<User> = this.state.select('user');
   boards$: Observable<Board[]>;
-  newBoardName: string;
+  newBoardForm = new FormGroup({
+    boardName: new FormControl(null, Validators.required)
+  });
+  private destroy = new Subject();
 
   constructor(private boardService: BoardService,
     private state: AppState,
     private route: ActivatedRoute,
     private router: Router,
-    private toast: Toast) { }
+    private toast: Toast,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.boards$ = this.route.data.pipe(map(data => data.user.boards));
   }
 
+  ngOnDestroy() {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
   async addBoard() {
     const user = this.state.get('user');
     try {
-      const board = await this.boardService.createBoard({ userId: user._id, boardName: this.newBoardName })
+      const { boardName } = this.newBoardForm.value;
+      const board = await this.boardService.createBoard({ userId: user._id, boardName })
         .toPromise();
       this.router.navigate(['/board', board._id]);
     } catch (err) {
       this.toast.show('An error occured', { type: 'error' });
     }
+  }
+
+  createBoardPopup() {
+    this.dialog.open(this.newBoardPopupTpl).beforeClosed().pipe(
+      takeUntil(this.destroy),
+      take(1)
+    ).subscribe(() => this.newBoardForm.reset());
   }
 
 }
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HomeResolve } from './home.resolve';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule, MatListModule, MatCardModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatCardModule, MatDialogModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     HomeRoutingModule,
     RouterModule,
     MatButtonModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatDialogModule,
+    MatInputModule
   ],
   providers: [HomeResolve]
 })
